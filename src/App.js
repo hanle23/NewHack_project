7323406c3b191a150837a025e4bed8f2ddfe46cc
Sacha Arseneault <sacha.arseneault@gmail.com>
2022-11-06 01:36:47 -0500

    app.js update

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import History from "./components/history";
 
 function App() {
   return (
-    /*
     <div className="main_page">
       <Menu />
     </div>
-    
+    /*
     <div className="secondary_page">
       <Message />
     </div>
-    */
+    
     <div className="secondary_page">
       <History />
     </div>
+    */
   );
 }
 
